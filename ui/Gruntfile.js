6baa4ca1113f1e8417d8f36884ad104988828e45
Justin Woodcock <justin@digitaluprisingdesign.com>
2015-01-12 09:21:58 -0800

    fixing prod build items

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -368,8 +368,8 @@ module.exports = function(grunt) {
             ],
             dist: [
                 'compass:dist',
-                'imagemin',
-                'svgmin'
+                //'imagemin',
+                //'svgmin'
             ]
         },
 
