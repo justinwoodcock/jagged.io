d42b20f58360df683af64b87020bcad3e40a269c
Justin Woodcock <justin@digitaluprisingdesign.com>
2015-01-12 08:49:43 -0800

    fixing prod build items

--- ui/Gruntfile.js
+++ ui/Gruntfile.js
@@ -339,7 +339,7 @@ module.exports = function(grunt) {
                         '*.{ico,png,txt}',
                         '.htaccess',
                         '*.html',
-                        'components/{,*/}*.html',
+                        'components/**/*.html',
                         'images/{,*/}*.{webp}',
                         'font/{,*/}*.*'
                     ]
