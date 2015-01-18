e77f4f4bca7c479022245b0ae2a0d73c02d32e5a
Justin Woodcock <justin@digitaluprisingdesign.com>
2015-01-17 18:07:07 -0800

    implement loader in work state, update some styling

--- ui/app/components/work/work.controller.js
+++ ui/app/components/work/work.controller.js
@@ -2,10 +2,12 @@
 
 jagged.controller('WorkController', ['$scope', 'JaggedFactory',
     function($scope, JaggedFactory) {
-
+        $scope.loading = true;
+        console.log($scope);
         JaggedFactory.createEntity('project').then(function(data) {
             $scope.ProjectEntity = data;
             $scope.projects = data.plain();
+            //$scope.loading = false;
         });
     }
 ]);
